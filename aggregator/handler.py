7793a429eb99732184a0b87b80c7c46718198d97
tremperteco <tremper@teco.edu>
2020-09-10 17:09:04 +0200

    fixed aggregator, should work now

--- aggregator/handler.py
+++ aggregator/handler.py
@@ -34,19 +34,41 @@ def getdata(initiallink):
             except:
                 morenextlinks=False;
                 break
+            if("$top=" in link):
+                morenextlinks=False;
     return data
 
+
+def replaceTimes(thequery,startquery,endquery):
+
+    starttrue=startquery
+    endtrue=endquery
+    
+    for el in thequery.split("$"): 
+        if("filter=resultTime" in el):
+            for time in el.split("=")[1].split("and"):
+                if("%20le%20" in time):
+                    endstamp=time.split("resultTime%20le%20")[1].split("Z")[0] + "Z"
+                    if(pd.to_datetime(endstamp) > pd.to_datetime(endquery)):
+                        endtrue=endquery
+                    else:
+                        endtrue=endstamp
+                elif("%20gt%20" in time):
+                    startstamp=time.split("resultTime%20gt%20")[1].split("Z")[0] + "Z"
+                    if(pd.to_datetime(startstamp) < pd.to_datetime(startquery)):
+                        starttrue=startquery
+                    else:
+                        starttrue=startstamp
+    return "$".join([("filter=resultTime%20gt%20" + starttrue +  "%20and%20" + "resultTime%20le%20" + endtrue + "&$count=true") if ("filter=resultTime" in x) else x for x in thequery.split("$")])
 
 def handle(event, context):
     
     t = event.query["$aggregate"]
     realkeys = list(filter(lambda x: x != "$aggregate", event.query.keys()))
     urlquery = "&".join(list(map(lambda x: x + "=" + event.query[x], realkeys)))
-    print(t)
     
     url = "https://api.smartaq.net/v1.0"
     frostquery=url + event.path + "?" + urlquery
-    
     
     freq=str(t) + "T"
     data=getdata(frostquery)
@@ -64,18 +86,17 @@ def handle(event, context):
         }
     
     #aggregate the data
-    agg=data.resample(freq).agg([np.sum, np.mean, np.std])
-    agg["count"]=(agg["sum"]/agg["mean"]).apply(int)
+    agg=data.resample(freq, label='right', closed='right').agg([np.sum, np.mean, np.std])
+    agg["count"]=agg["sum"]/agg["mean"]
     
     #output the result as a dataframe
-    result=pd.DataFrame({"result": list(agg["mean"]),"resultTime": list(agg.index.map(lambda x: x.isoformat().split("+")[0] + ".000Z")),"phenomenonTime": list(agg.index.map(lambda x: (x-pd.Timedelta(str(t) + "m")).isoformat().split("+")[0] + ".000Z" + "/" + x.isoformat().split("+")[0] + ".000Z")), "parameters": json.loads(agg[["count","std"]].to_json(orient="records"))})
-    
-    
+    result=pd.DataFrame({"result": list(round(agg["mean"],2)),"resultTime": [x.isoformat().split("+")[0] + ".000Z" for x in agg.index],"phenomenonTime": [(x-pd.Timedelta(str(t) + "m")).isoformat().split("+")[0] + ".000Z" + "/" + x.isoformat().split("+")[0] + ".000Z" for x in agg.index], "parameters": json.loads(agg[["count","std"]].apply(lambda x: round(x,3)).to_json(orient="records")),"Observations@iot.navigationLink": [replaceTimes(frostquery,(x-pd.Timedelta(str(t) + "m")).isoformat().split("+")[0] + ".000Z",x.isoformat().split("+")[0] + ".000Z") for x in agg.index]})
     
     return {
         "statusCode": 200,
-        "body": result.to_json(orient='records'), 
+        "body": '{"value":' + result.to_json(orient='records') + '}', 
         "headers": {
-            "Content-Type": "application/json"
+            "access-control-allow-origin": "*",
+            "content-type": "application/json;charset=UTF-8"
         }
     }
